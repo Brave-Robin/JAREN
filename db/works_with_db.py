b7fbdd392d91ba95d8d2c310d16fdeb749d63af8
Germych <gerasim.80v@mail.ru>
2023-12-25 18:58:03 +0100

    add function get_next_id

--- db/works_with_db.py
+++ db/works_with_db.py
@@ -6,13 +6,21 @@ conn = psycopg2.connect("host=127.0.0.1 port=5432 dbname=postgres user=postgres 
 
 cur = conn.cursor()
 
+
 # cur.execute("CREATE TABLE todo_germych (id serial PRIMARY KEY, text text NOT NULL, due_date date, "
 #             "blocked_by int, user_id int);")
 
+# cur.execute("INSERT INTO todo_germych VALUES (new_id, 'CZ');")
 
-# cur.execute("INSERT INTO todo_germych VALUES (5, 'MechJeb 2, Kerbal Interstellar Extended, Kerbal Engineer Redux');")
+
+def get_next_id():
+    cur.execute("SELECT MAX(id) FROM todo_germych;")
+    note_ids = cur.fetchall()
+    return note_ids[0][0] + 1
+
+print(get_next_id())
 
 
-cur.execute("UPDATE todo_germych SET due_date='12-21-2023' WHERE id=5;")
+# cur.execute("UPDATE todo_germych SET due_date='12-21-2023' WHERE id=5;")
 
-conn.commit()
+
