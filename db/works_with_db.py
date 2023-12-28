f8f5f3ab68b75c31e71e736704579f07406aff0c
ikaa <ikaa.shaverdashvili@gmail.com>
2023-12-28 19:25:31 +0400

    new function (new_task)

--- db/works_with_db.py
+++ db/works_with_db.py
@@ -2,9 +2,24 @@
 
 import psycopg2
 
-conn = psycopg2.connect("host=127.0.0.1 port=5432 dbname=test user=postgres password=mysecretpassword target_session_attrs=read-write")
+conn = psycopg2.connect("host=127.0.0.1 port=5432 dbname=todo user=postgres password=mysecretpassword target_session_attrs=read-write")
 
 cur = conn.cursor()
 
-cur.execute("INSERT INTO todos VALUES (2, 'test text again');")
-conn.commit()
+
+def get_next_id():
+    cur.execute("SELECT MAX(id) FROM todos;")
+    note_ids = cur.fetchall()
+    return note_ids[0][0] + 1
+
+
+def new_task():
+    print('enter new task: ')
+    task = input()
+    new_id = get_next_id()
+    cur.execute(f"INSERT INTO todos Values({new_id}, '{task}');")
+    return conn.commit()
+print(new_task())
+
+# cur.execute("INSERT INTO todos VALUES (2, 'test text again');")
+# conn.commit()
