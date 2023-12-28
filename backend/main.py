56def07133c713f735b9444125eebc6004ddb1b9
Germych <gerasim.80v@mail.ru>
2023-12-28 18:58:40 +0100

    deploy problems

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,26 @@
+import psycopg2
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
+conn = psycopg2.connect(
+    "host=127.0.0.1 "
+    "port=5432 "
+    "dbname=todo "
+    "user=postgres "
+    "password=mysecretpassword "
+    "target_session_attrs=read-write"
+)
+cur = conn.cursor()
+
+
+def get_all_notes(count=5):
+    data = cur.execute(f"select id,text,due_date,blocked_by,user_id from todo_germych limit {count};").fechall()
+    return data
 
 
 @app.get("/")
 async def root():
     # return {"message": "Hello World"}
-    return {"age": "24 years old", "home": {"country": "Niue", "address": "272 example street"}, "friends": ["Dora, Veronika", "Katleen, Phaedra", "Drucill, Anne-marie", "Starlene, Lenee", "Carroll, Callida"]}
+    return get_all_notes()
--- db/works_with_db.py
+++ db/works_with_db.py
@@ -6,6 +6,16 @@ conn = psycopg2.connect("host=127.0.0.1 port=5432 dbname=todo user=postgres pass
 
 cur = conn.cursor()
 
+# zapros = cur.execute(f"SELECT id,text,due_date,blocked_by,user_id FROM todo_germych LIMIT 5;")
+zapros = cur.execute("SELECT * FROM todo_germych")
+print(zapros)
+print(type(zapros))
+# def create_note(id, text, due_date='NULL', blocked_by='NULL', user_id='NULL'):
+#     cur.execute(f"INSERT INTO todo_germych Values({id}, '{text}', '{due_date}', {blocked_by}, {user_id});")
+#     return conn.commit()
+#
+#
+# create_note(8,'Марк_Лутц_Изучаем_Python_5_издание__том_1_2019.pdf', '01-12-2024', blocked_by="NULL")
 
 # cur.execute("CREATE TABLE todo_germych (id serial PRIMARY KEY, text text NOT NULL, due_date date, "
 #             "blocked_by int, user_id int);")
@@ -15,7 +25,7 @@ cur = conn.cursor()
 #             "ale také cenou. Stojí jako nová Fabia', '01-01-2024',NULL,NULL);")
 
 
-# cur.execute("INSERT INTO todo_germych (id, text, due_date, blocked_by, user_id) VALUES (2, 'Моды для КСП', NULL, "
+# cur.execute("INSERT INTO todo_germych (id, text, due_date, blocked_by, user_id) VALUES (25, 'Моды для КСП', NULL, "
 #             "NULL, NULL);")
 
 
@@ -27,11 +37,11 @@ cur = conn.cursor()
 #             "NULL, NULL);")
 
 
-cur.execute("INSERT INTO todo_germych (id, text, due_date, blocked_by, user_id) VALUES (5, 'Пехота: стургийский ратник', NULL, "
-            "4, NULL);")
-
-
-conn.commit()
+# cur.execute("INSERT INTO todo_germych (id, text, due_date, blocked_by, user_id) VALUES (5, 'Пехота: стургийский ратник', NULL, "
+#             "4, NULL);")
+#
+#
+# conn.commit()
 
 # def get_next_id():
 #     cur.execute("SELECT MAX(id) FROM todo_germych;")
