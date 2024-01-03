24412840842912b3968d41a241a630fe7ce5f738
Maria Maksimova <drogzed@gmail.com>
2024-01-03 22:58:43 +0400

    HW 21-12-2023

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,42 @@
+import psycopg2
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
+conn = psycopg2.connect(
+    "host=127.0.0.1 "
+    "port=5432 "
+    "dbname=todo "
+    "user=postgres "
+    "password=mysecretpassword "
+    "target_session_attrs=read-write"
+)
+cur = conn.cursor()
+
+
+def get_all_notes(count=5):
+    cur.execute(f"select id, text, due_date, blocked_by, user_id from todos limit {count}")
+    data = cur.fetchall()
+    return data
+
+
+def get_minimal_available_id():
+    cur.execute('SELECT MAX(id) FROM todos')
+    minimal_id = cur.fetchone()[0]
+    return minimal_id + 1
 
 
 @app.get("/")
 async def root():
     # return {"message": "Hello World"}
-    return {"age": "24 years old", "home": {"country": "Niue", "address": "272 example street"}, "friends": ["Dora, Veronika", "Katleen, Phaedra", "Drucill, Anne-marie", "Starlene, Lenee", "Carroll, Callida"]}
+    return get_all_notes()
+
+
+"""
+add column status. Execute in dbeaver
+
+--ALTER TABLE todos
+--ADD todo_status boolean NOT NULL DEFAULT False;
+
+"""
