de08b780c700b542308f201fce3877b638f4242f
Brave-Robin <117515256+Brave-Robin@users.noreply.github.com>
2024-01-03 23:59:38 +0400

    Docker compose mm (#3)
    * HW 21-12-2023
    
    * RDY for merge
    
    ---------
    
    Co-authored-by: Maria Maksimova <drogzed@gmail.com>

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,45 @@
+""" to_do backend part """
+import psycopg2
 from fastapi import FastAPI
+
 
 app = FastAPI()
 
+conn = psycopg2.connect(
+    "host=127.0.0.1 "
+    "port=5432 "
+    "dbname=todo "
+    "user=postgres "
+    "password=mysecretpassword "
+    "target_session_attrs=read-write"
+)
+cur = conn.cursor()
+
+
+def get_all_notes(count: int = 5) -> list:
+    """
+    :param count: select limit from DB
+    :return: list of lists
+    """
+    cur.execute(f"select id, text, due_date, blocked_by, user_id from todos limit {count}")
+    data = cur.fetchall()
+    return data
+
+
+def get_minimal_available_id() -> int:
+    """
+    :param: none
+    :return: integer next minimal ID
+    """
+    cur.execute('SELECT MAX(id) FROM todos')
+    minimal_id = cur.fetchone()[0]
+    return minimal_id + 1
+
 
 @app.get("/")
-async def root():
-    # return {"message": "Hello World"}
-    return {"age": "24 years old", "home": {"country": "Niue", "address": "272 example street"}, "friends": ["Dora, Veronika", "Katleen, Phaedra", "Drucill, Anne-marie", "Starlene, Lenee", "Carroll, Callida"]}
+async def root() -> list:
+    """
+    :param: none
+    :return: all notes
+    """
+    return get_all_notes()
