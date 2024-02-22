e6d26d49cbafdb9b9dbafb4067db9900fdc20d73
ikaa <ikaa.shaverdashvili@gmail.com>
2024-02-22 13:32:10 +0400

    changes in conventor.py

--- utils/convertor.py
+++ utils/convertor.py
@@ -3,13 +3,24 @@
 import urllib.parse
 
 
-input_string = input("Text to code: ")
+def text_converter():
+    input_string = input("Text to code: ")
+    text = urllib.parse.quote(input_string, safe='')
+    return text
 
-print(urllib.parse.quote(input_string, safe=''))
+def new_request():
+    input_due_date = input("(yyyy-mm-dd): ")
+    input_blocked_by = input("blocked by: ")
+    input_status = input("status: (true/false)")
+    input_user_id = input("user id: ")
+    new_cmnd = f"curl.exe -X 'PUT' 'http://127.0.0.1:8000/create/?todo_text={text_converter()}&todo_due_date={input_due_date}&todo_blocked_by={input_blocked_by}&todo_status={input_status}&todo_user_id={input_user_id}' -H 'accept: application/json'"
+    return new_cmnd
+
+print(new_request())
 
 """
 TODO: write script for create valid curl-request like a:
-curl.exe -X 'PUT' 'http://127.0.0.1:8000/create/?todo_text=%D0%BD%D0%BE%D0%B2%D1%8B%D0%B9%20todo%20%D0%B8%D0%B7%20%D1%83%D1%82%D0%B8%D0%BB%D0%B8%D1%82%D1%8B&todo_due_date=2024-12-30&todo_blocked_by=0&todo_status=false&todo_user_id=1000' -H 'accept: application/json'
+curl.exe -X 'PUT' 'http://127.0.0.1:8000/create/?todo_text=1%83%D1%82%D0%B8%D0%BB%D0%B8%D1%82%D1%8B&todo_due_date=2024-12-30&todo_blocked_by=0&todo_status=false&todo_user_id=1000' -H 'accept: application/json'
 with all fields!
 return: curl requests command
 """
